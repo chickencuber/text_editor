985047ff2b12752c782ebe469ff79cf239cb803d
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-28 00:28:49 -0500

    added simple text editing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,304 @@
 pub mod terminal;
+use std::vec;
+
 use terminal::{
-    Color,
-    set_color,
-    reset_color,
-    set_background,
-    clear,
-    get_cursor,
+    clear, disable, enable, key, reset_color, set_background, set_color, set_cursor,
+    set_cursor_style, Color, CursorStyle, KeyCode, KeyEventKind, Pos,
 };
 
 fn main() {
+    reset_color();
     clear();
-    set_color(Color::Red);
-    println!("Hello, Red!");
-    set_color(Color::Blue);
-    println!("Hello, Blue!");
-   set_background(Color::White);
-   set_color(Color::Black);
-   println!("Hello");
-   reset_color();
-   let pos = get_cursor();
-   println!("x: {}, y: {}, ", pos.x, pos.y);
+    enable();
+    let mut exit = false;
+    let mut mode = Modes::Normal;
+    let mut pos = Pos::from(0, 0);
+    let mut text: Vec<Vec<char>> = vec![Vec::new()];
+    loop {
+        if exit {
+            break;
+        }
+        update(&mut exit, &mut mode, &mut pos, &mut text);
+    }
+    disable();
+    reset_color();
+    clear();
+    set_cursor_style(CursorStyle::DefaultUserShape);
+}
+
+enum Modes {
+    Normal,
+    Insert,
+    Replace,
+}
+
+fn update(exit: &mut bool, mode: &mut Modes, pos: &mut Pos, text: &mut Vec<Vec<char>>) {
+    show_text(text);
+    set_cursor(pos.to_pos());
+    match *mode {
+        Modes::Normal => normal(exit, mode, pos, text),
+        Modes::Insert => insert(mode, pos, text),
+        Modes::Replace => replace(mode, pos, text),
+    }
+}
+
+fn normal(exit: &mut bool, mode: &mut Modes, pos: &mut Pos, text: &mut Vec<Vec<char>>) {
+    set_cursor_style(CursorStyle::BlinkingBlock);
+    if let Some(k) = key() {
+        if let Some(key) = k.filter(KeyEventKind::Release) {
+            match key.key {
+                KeyCode::Char('q') => *exit = true,
+                KeyCode::Char('i') => *mode = Modes::Insert,
+                KeyCode::Insert => *mode = Modes::Insert,
+                KeyCode::Left => {
+                    if pos.x != 0 {
+                        pos.x -= 1;
+                    }
+                }
+                KeyCode::Right => {
+                    if pos.x < x_size(text, pos.y) {
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Up => {
+                    if pos.y != 0 {
+                        pos.y -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if pos.y < (text.len() - 1).try_into().expect("conversion failed") {
+                        pos.y += 1;
+                    }
+                }
+                KeyCode::Home => pos.x = 0,
+                KeyCode::End => pos.x = x_size(text, pos.y),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn insert(mode: &mut Modes, pos: &mut Pos, text: &mut Vec<Vec<char>>) {
+    set_cursor_style(CursorStyle::BlinkingBar);
+    if let Some(k) = key() {
+        if let Some(key) = k.filter(KeyEventKind::Release) {
+            match key.key {
+                KeyCode::Insert => *mode = Modes::Replace,
+                KeyCode::Esc => *mode = Modes::Normal,
+                KeyCode::Char(c) => {
+                    let l = text.get_mut(usize::from(pos.y));
+                    if let Some(line) = l {
+                        line.insert(usize::from(pos.x), c);
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    if let Some(l) = text.get_mut(usize::from(pos.y)) {
+                        let h = l.drain(0..usize::from(pos.x)).collect();
+                        let len = l.len();
+                        set_cursor(Pos::from(0, pos.y));
+                        print!("{}", "     ".repeat(len));
+                        text.insert(usize::from(pos.y), h);
+                        pos.y += 1;
+                        pos.x = 0;
+                    }
+                }
+                KeyCode::Backspace => {
+                    let mut a: Option<Vec<char>> = None;
+                    if let Some(l) = text.get_mut(usize::from(pos.y)) {
+                        if pos.x == 0 {
+                            if (pos.y != 0) {
+                                let len = l.len();
+                                a = Some(l.clone());
+                                text.drain(usize::from(pos.y)..usize::from(pos.y + 1));
+                                set_cursor(Pos::from(0, pos.y));
+                                print!("{}", "     ".repeat(len));
+                                set_cursor(Pos::from(
+                                    0,
+                                    (text.len()).try_into().expect("conversion failed"),
+                                ));
+                                print!(
+                                    "{}",
+                                    "     ".repeat(usize::from(x_size(
+                                        text,
+                                        (text.len() - 1).try_into().expect("conversion failed")
+                                    )))
+                                );
+                                pos.y -= 1;
+                                pos.x = x_size(text, pos.y);
+                            }
+                        } else {
+                            let len = l.len();
+                            l.drain(usize::from(pos.x - 1)..usize::from(pos.x));
+                            set_cursor(Pos::from(0, pos.y));
+                            print!("{}", "     ".repeat(len));
+                            pos.x -= 1;
+                        }
+                    }
+                    if let Some(add) = a {
+                        if let Some(pl) = text.get_mut(usize::from(pos.y)) {
+                            pl.extend(add.iter())
+                        }
+                    }
+                }
+                KeyCode::Tab => {
+                    let l = text.get_mut(usize::from(pos.y));
+                    if let Some(line) = l {
+                        line.insert(usize::from(pos.x), ' ');
+                        pos.x += 1;
+                        line.insert(usize::from(pos.x), ' ');
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Left => {
+                    if pos.x != 0 {
+                        pos.x -= 1;
+                    }
+                }
+                KeyCode::Right => {
+                    if pos.x < x_size(text, pos.y) {
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Up => {
+                    if pos.y != 0 {
+                        pos.y -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if pos.y < (text.len() - 1).try_into().expect("conversion failed") {
+                        pos.y += 1;
+                    }
+                }
+                KeyCode::Home => pos.x = 0,
+                KeyCode::End => pos.x = x_size(text, pos.y),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn replace(mode: &mut Modes, pos: &mut Pos, text: &mut Vec<Vec<char>>) {
+    set_cursor_style(CursorStyle::BlinkingUnderScore);
+    if let Some(k) = key() {
+        if let Some(key) = k.filter(KeyEventKind::Release) {
+            match key.key {
+                KeyCode::Insert => *mode = Modes::Insert,
+                KeyCode::Esc => *mode = Modes::Normal,
+                KeyCode::Char(c) => {
+                    let l = text.get_mut(usize::from(pos.y));
+                    if let Some(line) = l {
+                        line.insert(usize::from(pos.x), c);
+                        if line.len() != usize::from(pos.x + 1) {
+                            line.remove(usize::from(pos.x + 1));
+                        }
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    if let Some(l) = text.get_mut(usize::from(pos.y)) {
+                        let h = l.drain(0..usize::from(pos.x)).collect();
+                        let len = l.len();
+                        set_cursor(Pos::from(0, pos.y));
+                        print!("{}", "     ".repeat(len));
+                        text.insert(usize::from(pos.y), h);
+                        pos.y += 1;
+                        pos.x = 0;
+                    }
+                }
+                KeyCode::Backspace => {
+                    let mut a: Option<Vec<char>> = None;
+                    if let Some(l) = text.get_mut(usize::from(pos.y)) {
+                        if pos.x == 0 {
+                            if (pos.y != 0) {
+                                let len = l.len();
+                                a = Some(l.clone());
+                                text.drain(usize::from(pos.y)..usize::from(pos.y + 1));
+                                set_cursor(Pos::from(0, pos.y));
+                                print!("{}", "     ".repeat(len));
+                                set_cursor(Pos::from(
+                                    0,
+                                    (text.len()).try_into().expect("conversion failed"),
+                                ));
+                                print!(
+                                    "{}",
+                                    "     ".repeat(usize::from(x_size(
+                                        text,
+                                        (text.len() - 1).try_into().expect("conversion failed")
+                                    )))
+                                );
+                                pos.y -= 1;
+                                pos.x = x_size(text, pos.y);
+                            }
+                        } else {
+                            let len = l.len();
+                            l.drain(usize::from(pos.x - 1)..usize::from(pos.x));
+                            set_cursor(Pos::from(0, pos.y));
+                            print!("{}", "     ".repeat(len));
+                            pos.x -= 1;
+                        }
+                    }
+                    if let Some(add) = a {
+                        if let Some(pl) = text.get_mut(usize::from(pos.y)) {
+                            pl.extend(add.iter())
+                        }
+                    }
+                }
+                KeyCode::Tab => {
+                    let l = text.get_mut(usize::from(pos.y));
+                    if let Some(line) = l {
+                        line.insert(usize::from(pos.x), ' ');
+                        if line.len() != usize::from(pos.x + 1) {
+                            line.remove(usize::from(pos.x + 1));
+                        }
+                        pos.x += 1;
+                        line.insert(usize::from(pos.x), ' ');
+                        if line.len() != usize::from(pos.x + 1) {
+                            line.remove(usize::from(pos.x + 1));
+                        }
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Left => {
+                    if pos.x != 0 {
+                        pos.x -= 1;
+                    }
+                }
+                KeyCode::Right => {
+                    if pos.x < x_size(text, pos.y) {
+                        pos.x += 1;
+                    }
+                }
+                KeyCode::Up => {
+                    if pos.y != 0 {
+                        pos.y -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if pos.y < (text.len() - 1).try_into().expect("conversion failed") {
+                        pos.y += 1;
+                    }
+                }
+                KeyCode::Home => pos.x = 0,
+                KeyCode::End => pos.x = x_size(text, pos.y),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn show_text(text: &Vec<Vec<char>>) {
+    for (y, line) in text.iter().enumerate() {
+        set_cursor(Pos::from(0, y.try_into().expect("conversion failed")));
+        print!("{}", String::from_iter(line));
+    }
+}
+
+fn x_size(text: &Vec<Vec<char>>, y: u16) -> u16 {
+    let l = text.get(usize::from(y));
+    if let Some(line) = l {
+        return (line.len()).try_into().expect("conversion failed");
+    } else {
+        return 0;
+    }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,13 +1,25 @@
-use crossterm::{
-    execute,
-    style::{ResetColor, SetForegroundColor, SetBackgroundColor},
-    terminal::{Clear, ClearType},
-    cursor::{MoveTo, position},
-};
+pub use crossterm::event::KeyCode;
+pub use crossterm::event::KeyEventKind;
 pub use crossterm::style::Color;
+use crossterm::{
+    cursor::{position, MoveTo, SetCursorStyle},
+    event::{self, Event, KeyEvent},
+    execute,
+    style::{ResetColor, SetBackgroundColor, SetForegroundColor},
+    terminal::{disable_raw_mode, enable_raw_mode},
+    terminal::{Clear, ClearType},
+};
 
-pub fn set_cursor(x: u16, y: u16) {
-    execute!(std::io::stdout(), MoveTo(x, y)).expect("Failed to set cursor position");
+pub fn set_cursor(pos: Pos) {
+    execute!(std::io::stdout(), MoveTo(pos.x, pos.y)).expect("Failed to set cursor position");
+}
+
+pub fn enable() {
+    enable_raw_mode().expect("Failed to enable raw mode");
+}
+
+pub fn disable() {
+    disable_raw_mode().expect("Failed to disable raw mode");
 }
 
 pub fn get_cursor() -> Pos {
@@ -15,12 +27,32 @@ pub fn get_cursor() -> Pos {
         Ok(pos) => pos,
         Err(_) => (0, 0),
     };
-    return Pos {x: x, y: y};
+    return Pos { x, y };
 }
 
 pub struct Pos {
     pub x: u16,
     pub y: u16,
+}
+
+impl Pos {
+    pub fn from(x: u16, y: u16) -> Self {
+        return Self { x, y };
+    }
+    pub fn from_tuple((x, y): (u16, u16)) -> Self {
+        return Self::from(x, y);
+    }
+    pub fn to_tuple(&self) -> (u16, u16) {
+        return (self.x, self.y);
+    }
+    pub fn set(&mut self, pos: Self) -> &Self {
+        self.x = pos.x;
+        self.y = pos.y;
+        return self;
+    }
+    pub fn to_pos(&self) -> Self {
+        return Self {x: self.x, y: self.y};
+    }
 }
 
 pub fn set_color(color: Color) {
@@ -37,5 +69,62 @@ pub fn reset_color() {
 
 pub fn clear() {
     execute!(std::io::stdout(), Clear(ClearType::All)).expect("Failed to clear terminal");
-    set_cursor(0, 0);
+    print!("\x1B[2J");
+    set_cursor(Pos::from(0, 0));
+}
+
+pub struct Key {
+    pub kind: KeyEventKind,
+    pub key: KeyCode,
+}
+
+impl Key {
+    pub fn filter(&self, filter: KeyEventKind) -> Option<&Self> {
+        if self.kind == filter {
+            return None;
+        }
+        return Some(self);
+    }
+    pub fn only(&self, filter: KeyEventKind) -> Option<&Self> {
+        if self.kind == filter {
+            return Some(self);
+        }
+        return None;
+    }
+}
+
+pub fn key() -> Option<Key> {
+    if let Event::Key(KeyEvent { code, kind, .. }) = event::read().expect("Failed to read event") {
+        return Some(Key { kind, key: code });
+    } else {
+        return None;
+    }
+}
+
+pub enum CursorStyle {
+    DefaultUserShape,
+    BlinkingBlock,
+    SteadyBlock,
+    BlinkingUnderScore,
+    SteadyUnderScore,
+    BlinkingBar,
+    SteadyBar,
+}
+
+impl CursorStyle {
+    pub fn to_cursor_style(&self) -> SetCursorStyle {
+        match self {
+            Self::DefaultUserShape => SetCursorStyle::DefaultUserShape,
+            Self::BlinkingBlock => SetCursorStyle::BlinkingBlock,
+            Self::SteadyBlock => SetCursorStyle::SteadyBlock,
+            Self::BlinkingUnderScore => SetCursorStyle::BlinkingUnderScore,
+            Self::SteadyUnderScore => SetCursorStyle::SteadyUnderScore,
+            Self::BlinkingBar => SetCursorStyle::BlinkingBar,
+            Self::SteadyBar => SetCursorStyle::SteadyBar,
+        }
+    }
+}
+
+pub fn set_cursor_style(style: CursorStyle) {
+    execute!(std::io::stdout(), style.to_cursor_style()).expect("Failed to set the Cursor");
 }
